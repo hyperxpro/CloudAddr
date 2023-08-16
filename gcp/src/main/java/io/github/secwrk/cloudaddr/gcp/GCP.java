67c998c276729e4e2cff240a8297109a3f68b1ec
Aayush Atharva <aayush@shieldblaze.com>
2023-08-17 01:49:10 +0530

    Add GCP Support

---
+++ gcp/src/main/java/io/github/secwrk/cloudaddr/gcp/GCP.java
@@ -0,0 +1,10 @@
+package io.github.secwrk.cloudaddr.gcp;
+
+import java.util.List;
+
+public record GCP (long syncToken, String creationTime, List<Prefix> prefixes) {
+
+    public record Prefix(String ipv4Prefix, String ipv6Prefix, String service, String scope) {
+        // NO-OP
+    }
+}
---
+++ gcp/src/main/java/io/github/secwrk/cloudaddr/gcp/Main.java
@@ -0,0 +1,71 @@
+package io.github.secwrk.cloudaddr.gcp;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.secwrk.cloudaddr.core.FileWriter;
+import io.github.secwrk.cloudaddr.core.IPv4AddressComparator;
+import io.github.secwrk.cloudaddr.core.IPv6AddressComparator;
+import io.github.secwrk.cloudaddr.core.Prefixes;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .header("Accept", "application/json")
+                .GET()
+                .uri(URI.create("https://www.gstatic.com/ipranges/cloud.json"))
+                .build();
+
+        HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        GCP gcp = objectMapper.readValue(httpResponse.body(), GCP.class);
+
+        // Remove duplicates and sort for IPv4
+        List<GCP.Prefix> ipv4Prefixes = gcp.prefixes().stream()
+                .filter(prefix -> prefix.ipv4Prefix() != null)
+                .collect(Collectors.toMap(GCP.Prefix::ipv4Prefix, Function.identity(), (p1, p2) -> p1))
+                .values()
+                .stream()
+                .sorted(Comparator.comparing(GCP.Prefix::ipv4Prefix, IPv4AddressComparator.INSTANCE))
+                .toList();
+
+        // Remove duplicates and sort for IPv6
+        List<GCP.Prefix> ipv6Prefixes = gcp.prefixes().stream()
+                .filter(prefix -> prefix.ipv6Prefix() != null)
+                .collect(Collectors.toMap(GCP.Prefix::ipv6Prefix, Function.identity(), (p1, p2) -> p1))
+                .values()
+                .stream()
+                .sorted(Comparator.comparing(GCP.Prefix::ipv6Prefix, IPv6AddressComparator.INSTANCE))
+                .toList();
+
+        // Map prefix to string
+        List<String> ipv4 = ipv4Prefixes.stream()
+                .map(GCP.Prefix::ipv4Prefix)
+                .toList();
+
+        // Map prefix to string
+        List<String> ipv6 = ipv6Prefixes.stream()
+                .map(GCP.Prefix::ipv6Prefix)
+                .toList();
+
+        // Write IPv6 prefixes to file
+        FileWriter.writeJsonFile("data/aws/gcp-ipv4.json", new Prefixes("GCP", ipv4));
+        FileWriter.writeTextFile("data/aws/gcp-ipv4.txt", new Prefixes("GCP", ipv4));
+
+        // Write IPv6 prefixes to file
+        FileWriter.writeJsonFile("data/aws/gcp-ipv6.json", new Prefixes("GCP", ipv6));
+        FileWriter.writeTextFile("data/aws/gcp-ipv6.txt", new Prefixes("GCP", ipv6));
+    }
+}
